ad2b5fab86a82174e43aea8fba0a6b80fc877c64
andrew lupton and Austin Roberts <austin@>
2018-05-23 10:35:25 -0700

    add initial files

---
+++ src/haiku.js
@@ -0,0 +1,10 @@
+class Haiku
+{
+  constructor(arg1, arg2, arg3)
+  {
+    this.arg1=arg1;
+    this.arg2=arg2;
+    this.arg3=arg3;
+  }
+
+}
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import 'bootstrap';
+import '/bootstrap/dist/css/bootstrap.min/css';
+import './styles.css';
+import $ from 'jquery';
+import { Haiku } from './haiku';
+
+
+
+$(document).ready(function() {
+  $('#form').submit(function(event) {
+    event.preventDefault();
+  });
+});
